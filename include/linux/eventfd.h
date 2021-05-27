e2d7c773410bda89728f2ddd6fb03685ba1bb439
Juri Lelli <juri.lelli@redhat.com>
2021-05-27 07:21:51 +0200

    Revert "eventfd: track eventfd_signal() recursion depth"
    This reverts commit b5e683d5cab8cd433b06ae178621f083cabd4f63.

--- include/linux/eventfd.h
+++ include/linux/eventfd.h
@@ -12,8 +12,6 @@
 #include <linux/fcntl.h>
 #include <linux/wait.h>
 #include <linux/err.h>
-#include <linux/percpu-defs.h>
-#include <linux/percpu.h>
 
 /*
  * CAREFUL: Check include/uapi/asm-generic/fcntl.h when defining
@@ -43,13 +41,6 @@ int eventfd_ctx_remove_wait_queue(struct eventfd_ctx *ctx, wait_queue_entry_t *w
 				  __u64 *cnt);
 void eventfd_ctx_do_read(struct eventfd_ctx *ctx, __u64 *cnt);
 
-DECLARE_PER_CPU(int, eventfd_wake_count);
-
-static inline bool eventfd_signal_count(void)
-{
-	return this_cpu_read(eventfd_wake_count);
-}
-
 #else /* CONFIG_EVENTFD */
 
 /*
@@ -76,11 +67,6 @@ static inline int eventfd_ctx_remove_wait_queue(struct eventfd_ctx *ctx,
 						wait_queue_entry_t *wait, __u64 *cnt)
 {
 	return -ENOSYS;
-}
-
-static inline bool eventfd_signal_count(void)
-{
-	return false;
 }
 
 static inline void eventfd_ctx_do_read(struct eventfd_ctx *ctx, __u64 *cnt)
