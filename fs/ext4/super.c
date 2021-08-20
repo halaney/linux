23e922723aa8ec895924b33a13ed4968d71133e9
Bill O'Donnell <bodonnel@redhat.com>
2021-08-20 09:46:32 -0500

    [fs] dax: mark tech preview
    Bugzilla: 1995338
    Upstream Status: RHEL only
    Tested: Local xfstests on ext4 and xfs with and without dax
    
    Make sure to taint the kernel with TAINT_TECH_PREVIEW when a file
    system is mounted with "-o dax".
    
    Signed-off-by: Bill O'Donnell <bodonnel@redhat.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -4800,6 +4800,7 @@ static int __ext4_fill_super(struct fs_context *fc, struct super_block *sb)
 	}
 
 	if (sbi->s_mount_opt & EXT4_MOUNT_DAX_ALWAYS) {
+		static bool printed = false;
 		if (ext4_has_feature_inline_data(sb)) {
 			ext4_msg(sb, KERN_ERR, "Cannot use DAX on a filesystem"
 					" that may contain inline data");
@@ -4809,6 +4810,10 @@ static int __ext4_fill_super(struct fs_context *fc, struct super_block *sb)
 			ext4_msg(sb, KERN_ERR,
 				"DAX unsupported by block device.");
 			goto failed_mount;
+		}
+		if (!printed) {
+			mark_tech_preview("ext4 direct access (dax)", NULL);
+			printed = true;
 		}
 	}
 
