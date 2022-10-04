c94219635dd713257a9b7abcf49598d2554effcc
Brian Masney <bmasney@redhat.com>
2022-10-04 10:26:38 -0400

    net: stmmac: support 88Q5072 mac switch
    Bugzilla: INTERNAL
    Upstream Status: https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/rh-patch/-/tree/rh-patch.lnx.1.0.r1-rel
    
    commit a611d6ced5fe5739bb3723c2da5778d931afca85
    Author: Ning Cai <quic_ncai@quicinc.com>
    Date:   Thu Feb 17 10:09:21 2022 -0800
    
        net: stmmac: support 88Q5072 mac switch
    
        Add support for 88Q5072 mac2mac switch.
        It requires to disable TX_CLK_PHASE_SHIFT.
        Append -emac1 to compatible string per dts change.
    
        Change-Id: Id5955ddb219292dd8582fdc40cb1d03a6cec4c2b
        Signed-off-by: Ning Cai <quic_ncai@quicinc.com>
    
    Signed-off-by: Andrew Halaney <ahalaney@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.h
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.h
@@ -52,4 +52,5 @@ struct qcom_ethqos {
 int ethqos_init_reqgulators(struct qcom_ethqos *ethqos);
 void ethqos_disable_regulators(struct qcom_ethqos *ethqos);
 int ethqos_init_gpio(struct qcom_ethqos *ethqos);
+void *qcom_ethqos_get_priv(struct qcom_ethqos *ethqos);
 #endif
