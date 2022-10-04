85a0fa5ab35806e8bf97f1efd3da465aa7ea7e66
Brian Masney <bmasney@redhat.com>
2022-10-04 10:26:39 -0400

    net: stmmac: SDx55: add pps signal and interrupts support
    Bugzilla: INTERNAL
    Upstream Status: https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/rh-patch/-/tree/rh-patch.lnx.1.0.r1-rel
    
    commit 131fd376cfeb367df1aa6cb4e914882bf33c22c0
    Author: Suraj Jaiswal <jsuraj@codeaurora.org>
    Date:   Wed Mar 17 15:30:09 2021 +0530
    
        net: stmmac: SDx55: add pps signal and interrupts support
    
        Add ioctl support to handle and generate pps siganal &
        interrupt.Add device nodes for pps interrupts to interact
        with user space.
    
        Change-Id: I401326443e69bfd0fb37986004bdc47f41dc57b7
        Acked-by: Nagarjuna Chaganti <nchagant@qti.qualcomm.com>
        Signed-off-by: Suraj Jaiswal <jsuraj@codeaurora.org>
    
    Signed-off-by: Andrew Halaney <ahalaney@redhat.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3273,6 +3273,8 @@ static int stmmac_hw_setup(struct net_device *dev, bool init_ptp)
 			netdev_warn(priv->dev, "PTP not supported by HW\n");
 		else if (ret)
 			netdev_warn(priv->dev, "PTP init failed\n");
+		else
+			ret = clk_set_rate(priv->plat->clk_ptp_ref, 96000000);
 	}
 
 	priv->eee_tw_timer = STMMAC_DEFAULT_TWT_LS;
@@ -5836,6 +5838,9 @@ static int stmmac_ioctl(struct net_device *dev, struct ifreq *rq, int cmd)
 		break;
 	case SIOCGHWTSTAMP:
 		ret = stmmac_hwtstamp_get(dev, rq);
+		break;
+	case SIOCDEVPRIVATE:
+		ret = priv->plat->handle_prv_ioctl(dev, rq, cmd);
 		break;
 	default:
 		break;
