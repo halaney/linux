099b893a02aeab83afb0294c1aa807a10bad804d
Shazad Hussain <quic_shazhuss@quicinc.com>
2023-03-01 21:30:14 +0530

    hack: clk: qcom: gcc: enable cam hf sf clocks
    this patch enables gcc_camera_hf_axi_clk and gcc_camera_sf_axi_clk.
    
    Change-Id: I4e9716e9001a797501b897a7ebb087fd932c83e8
    Signed-off-by: Shazad Hussain <quic_shazhuss@quicinc.com>

--- drivers/clk/qcom/gcc-lemans.c
+++ drivers/clk/qcom/gcc-lemans.c
@@ -1907,6 +1907,7 @@ static struct clk_branch gcc_camera_hf_axi_clk = {
 		.enable_mask = BIT(0),
 		.hw.init = &(const struct clk_init_data){
 			.name = "gcc_camera_hf_axi_clk",
+			.flags = CLK_IS_CRITICAL,
 			.ops = &clk_branch2_ops,
 		},
 	},
@@ -1922,6 +1923,7 @@ static struct clk_branch gcc_camera_sf_axi_clk = {
 		.enable_mask = BIT(0),
 		.hw.init = &(const struct clk_init_data){
 			.name = "gcc_camera_sf_axi_clk",
+			.flags = CLK_IS_CRITICAL,
 			.ops = &clk_branch2_ops,
 		},
 	},
