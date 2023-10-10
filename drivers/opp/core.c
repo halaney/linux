5cc91a939434d43b83f78c9a3b7c8218a8f6bb94
Ulf Hansson <ulf.hansson@linaro.org>
2023-10-10 07:48:39 -0700

    OPP: Switch to use dev_pm_domain_set_performance_state()
    To support performance scaling for any kinds of PM domains, let's move away
    from using the genpd specific API, dev_pm_genpd_set_performance_state(), to
    the common dev_pm_domain_set_performance_state().
    
    No intended functional impact at this point.
    
    Change-Id: I5fb4e421552290f55559a176c52699c1050339ad
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Patch-mainline: linux-arm-kernel @ 09/25/23 15:17
    Link: https://lore.kernel.org/all/20230925131715.138411-6-ulf.hansson@linaro.org/
    Signed-off-by: Nikunj Kela <quic_nkela@quicinc.com>

--- drivers/opp/core.c
+++ drivers/opp/core.c
@@ -901,7 +901,7 @@ static int _set_required_opp(struct device *dev, struct device *pd_dev,
 	if (!pd_dev)
 		return 0;
 
-	ret = dev_pm_genpd_set_performance_state(pd_dev, pstate);
+	ret = dev_pm_domain_set_performance_state(pd_dev, pstate);
 	if (ret) {
 		dev_err(dev, "Failed to set performance rate of %s: %d (%d)\n",
 			dev_name(pd_dev), pstate, ret);
